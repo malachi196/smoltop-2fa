1ec1e030f76eee3ada5b7050e311b7f07a0f01c4
malachi196 <malachiaaronwilson@gmail.com>
2026-08-12 15:47:50 -0500

    Apply license to source code

--- smoltop/__asciistuff.py
+++ smoltop/__asciistuff.py
@@ -1,4 +1,26 @@
-#this is simultaneously unecessary and absolutely required out of style :P
+# Copyright (C) 2026 @malachi196
+#
+# This file is part of SmolTOP
+#
+# SmolTOP is a Time-based One Time Pass (TOTP) 2 Factor Authentication (2FA)
+# tool that is designed to be very small (smol), and relatively lightweight. This 
+# helps make it portable and convenient at a moment's notice, without using too
+# many system resources.
+#
+# SmolTOP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SmolTOP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#this is simultaneously unecessary and absolutely required out of style and branding :P
 
 smollertopasciiart = """
            *+*                     
--- smoltop/cryptography.py
+++ smoltop/cryptography.py
@@ -1,4 +1,24 @@
-#backend cryptography file
+# Copyright (C) 2026 @malachi196
+#
+# This file is part of SmolTOP
+#
+# SmolTOP is a Time-based One Time Pass (TOTP) 2 Factor Authentication (2FA)
+# tool that is designed to be very small (smol), and relatively lightweight. This 
+# helps make it portable and convenient at a moment's notice, without using too
+# many system resources.
+#
+# SmolTOP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SmolTOP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import hashlib
 from time import time
